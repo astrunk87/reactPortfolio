ae768b81b01376676b6e2c87c0c21060f2b24cb4
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-11-08 16:30:38 -0600

    added the rest of the projects to the porfolio page with link working

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -19,7 +19,7 @@ function Portfolio() {
             <a target="_blank" href="https://gabe-velasquez.github.io/Whose_Hotter/"
             ><img className='classImg' src="src\assets\whoseHotterWebsite.png"  alt="WhoseHotter text with beach umbrella and sun"/></a> 
             <br></br>
-            <a className='repo' href='https://github.com/astrunk87/weatherApp'>  github repo for this project  </a>    
+            <a className='repo' href='https://github.com/astrunk87/weatherApp'>  github repo for Whose Hotter project  </a>    
                          {/*clickable pic with help from w3schools  */}
             </div>    
             <br></br>
@@ -28,17 +28,41 @@ function Portfolio() {
                 <br></br>
                 <a target='_blank' href="https://hidden-savannah-06081-4ae52891d71b.herokuapp.com/api/aboutRoute/about"><img className='classImg' src='src/assets/starbysCupCornerWebsite.png'></img></a>
                 <br></br>
-            <a className='repo' href='https://github.com/shinyuta/Project-2-UofMN'>  github repo for this project  </a> 
+            <a className='repo' href='https://github.com/shinyuta/Project-2-UofMN'>  github repo for Starby's Cup Corner project  </a> 
             </div>   
-        <p>
-        put something else here
-        </p>
-        <p>
-        put something else here.
-        </p>
-        <p>
-        put something else here
-        </p>
+            
+            <br></br>
+            
+            <div className='project'>
+                <a target='_blank' href="https://astrunk87.github.io/WorkDayScheduler/"><img className='classImg' src="src\assets\WorkDayScheduler.png" alt="workday scheduler"></img></a>
+                <br></br>
+            <a className='repo' href='https://github.com/astrunk87/WorkDayScheduler'>  github repo for Work Day Scheduler project  </a> 
+            </div>  
+
+            <br></br>            
+            
+            <div className='project'>
+                <a target='_blank' href="https://astrunk87.github.io/weatherApp/"><img className='classImg' src="src\assets\5dayForecast.png" alt="note taker"></img></a>
+                <br></br>
+            <a className='repo' href='https://github.com/astrunk87/weatherApp'>  github repo for Weather App project  </a> 
+            </div>
+            
+            <br></br>
+
+            <div className='project'>
+                <a target='_blank' href="https://notesbyas-2219e7dcb68d.herokuapp.com/"><img className='classImg' src="src\assets\noteTaker2.png" alt="note taker"></img></a>
+                <br></br>
+            <a className='repo' href='https://github.com/astrunk87/note_taker'>  github repo for Note Taker project  </a> 
+            </div>
+            
+            <br></br>
+            
+            <div className='project'>
+                <a target='_blank' href="https://astrunk87.github.io/mod4-quiz/"><img className='classImg' src="src\assets\codingQuiz.png" alt="note taker"></img></a>
+                <br></br>
+            <a className='repo' href='https://github.com/astrunk87/mod4-quiz'>  github repo for Coding Quiz project  </a> 
+            </div>
+        
       </section>
     </div>
   );
