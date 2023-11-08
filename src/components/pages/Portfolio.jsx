a1fcad44c083910ffc475cb96fcdd7c46d7b8802
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-11-08 15:48:11 -0600

    added github links, css styling for portfolio page

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -12,15 +12,23 @@ function Portfolio() {
   return (
     <div className='container'>
       <section className="section">
-        <h2>take a look at some of the projects I have created below</h2>
-          <div className='container'>
-                <img src="src\assets\whoseHotterLogo.svg"  alt="WhoseHotter text with beach umbrella and sun"/>
-                <a href="https://gabe-velasquez.github.io/Whose_Hotter/">Whose Hotter</a>              
+        <h2>Take a look at some of the projects I have created below</h2>
+          <div className='project'>      
+          <img className='classImg' src="src\assets\whoseHotterLogo.svg"  alt="WhoseHotter text with beach umbrella and sun"/>  
+          <br></br>        
+            <a target="_blank" href="https://gabe-velasquez.github.io/Whose_Hotter/"
+            ><img className='classImg' src="src\assets\whoseHotterWebsite.png"  alt="WhoseHotter text with beach umbrella and sun"/></a> 
+            <br></br>
+            <a className='repo' href='https://github.com/astrunk87/weatherApp'>  github repo for this project  </a>    
+                         {/*clickable pic with help from w3schools  */}
             </div>    
             <br></br>
-            <div className='container'>
-                <img src="src\assets\logo.png" alt="coffee cup with text Starbys Cup Corner"/>
-                <a href="https://hidden-savannah-06081-4ae52891d71b.herokuapp.com/api/aboutRoute/about">Starbys Cup Corner</a>
+            <div className='project'>
+                <img className='classImg' src="src\assets\logo.png" alt="coffee cup with text Starbys Cup Corner"/>
+                <br></br>
+                <a target='_blank' href="https://hidden-savannah-06081-4ae52891d71b.herokuapp.com/api/aboutRoute/about"><img className='classImg' src='src/assets/starbysCupCornerWebsite.png'></img></a>
+                <br></br>
+            <a className='repo' href='https://github.com/shinyuta/Project-2-UofMN'>  github repo for this project  </a> 
             </div>   
         <p>
         put something else here
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,8 +2,8 @@ export default function Resume() {
     return (
       <div>
         {/* <h1>Resume</h1> */}
-        <img className='classImg'src='src/assets/0001.jpg'></img>
-        <img src='src/assets/0002.jpg'className='classImg'></img>
+        <img className='classImg'src='src/assets/resumepg1.jpg'></img>
+        <img src='src/assets/resumepg2.jpg'className='classImg'></img>
         <div 
         className= 'd-flex align-items-center mt-5 justify-content-center'>
             <a className='btn btn-success' href='src/assets/alex-strunk-resume.pdf'>Download </a>            
