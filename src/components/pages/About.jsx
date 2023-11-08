f807a981bfe5df08c6d108211fc3f60c54c9cda5
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-11-08 17:42:57 -0600

    work on pic bug, not showing up in netlify set

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,8 +3,8 @@ function About() {
   return (
     <header className="header">
       <h1 >Alex Strunk </h1>
-      <img  src='src\assets\alexstrunk.jpg'></img>
-      <p className='bio'>An recent grad from a coding bootcamp who is outgoing, passionate, and experienced leader who is growth-motivated. Excels in a fast-paced environment while maintaining detailed organization and communication. Values a fun, diverse, and dedicated team. Extensive experience in inventory management, managerial duties, supply and demand relations, and customer service.</p>
+      <img  src='../../src/assets/alexstrunk.jpg'></img>
+      <p className='bio'>A recent grad from a coding bootcamp who is outgoing, passionate, and experienced leader who is growth-motivated. Excels in a fast-paced environment while maintaining detailed organization and communication. Values a fun, diverse, and dedicated team. Extensive experience in inventory management, managerial duties, supply and demand relations, and customer service.</p>
     </header>
   );
 }
